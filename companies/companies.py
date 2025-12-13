5ac42147a883c402af23bd3358df9976013f1824
Matthew Leong <matthewwjleong2006@gmail.com>
2025-12-13 14:40:53 +1100

    Added jobs and companies

--- companies/companies.py
+++ companies/companies.py
@@ -1,6 +1,36 @@
+class JobSupplier:
+    def __init__(self, )
+
 class Company:
-    def __init__(self, company_id: str, name: str, jobs: list):
+    def __init__(self, company_id: str, name: str, match_threshold: float, jobs: list):
         self.company_id = company_id
         self.name = name
         self.jobs = jobs
+
+
+    def find_job_for_candidate(self, candidate_skills: dict):
         
+
+
+class Job:
+    def __init__(self, job_id: str, title: str, description: str, weights: dict, maximum_pay: float, starting_pay: float):
+        self.job_id = job_id
+        self.title = title
+        self.description = description
+        self.weights = weights
+        self.maximum_pay = maximum_pay
+        self.starting_pay = starting_pay
+
+
+    def calculate_match(self, candidate_skills: dict) -> float:
+        total_weight = sum(self.weights.values())
+        if total_weight == 0:
+            return 0.0
+
+        match_score = 0.0
+        for skill, weight in self.weights.items():
+            candidate_skill_level = candidate_skills.get(skill, 0)
+            match_score += (candidate_skill_level * weight)
+
+        return match_score / total_weight
+    
--- services/service.py
+++ services/service.py
@@ -1,8 +1,10 @@
 from candidates.candidates import CandidateSupplier
+from companies.companies import JobSupplier, Job
 
 class Service:
     def __init__(self):
         self.candidate_supplier = CandidateSupplier()
+        self.job_supplier = JobSupplier()
 
     
     def add_candidate(self, candidate_id: str, password: str, name: str, skills: list):
@@ -11,3 +13,19 @@ class Service:
 
     def job_search(self, candidate_id: str):
         pass
+
+
+    def add_job(self, company_id: str, job_id: str, title: str, description: str, weights: dict, match_threshold: float, maximum_pay: float, starting_pay: float):
+
+        job = Job(
+            job_id=job_id,
+            title=title,
+            description=description,
+            weights=weights,
+            match_threshold=match_threshold,
+            maximum_pay=maximum_pay,
+            starting_pay=starting_pay
+        )
+
+        company = self.job_supplier.companies[company_id]
+        company.add_job(job)
